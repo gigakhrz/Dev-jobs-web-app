86e49728e4c67b5dc23c25a90e3f51768c97a69c
gigakhrz <kharazishvilig13@gmail.com>
2023-07-12 13:32:49 +0400

    add moreinfoSlice in store

--- src/features/store.ts
+++ src/features/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import lightModeSLice, { Light } from "./lightModeSLice";
 import pageSlice, { Page } from "./pageSlice";
 import DevJobsSlice, { DevJobInterface } from "./DevJobsSlice";
+import moreInfoSlice, { info } from "./moreInfo";
 const store = configureStore({
   reducer: {
     lightMode: lightModeSLice,
     page: pageSlice,
     devJob: DevJobsSlice,
+    moreInfo: moreInfoSlice,
   },
 });
 
@@ -14,6 +16,7 @@ export type RootState = {
   lightMode: Light;
   page: Page;
   devJob: DevJobInterface;
+  moreInfo: info;
 };
 
 export default store;
