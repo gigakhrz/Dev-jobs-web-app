0ec9985057cc8d63e62073f5b936963d4941ee43
gigakhrz <kharazishvilig13@gmail.com>
2023-08-09 11:37:08 +0400

    debug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ const Header = (): JSX.Element => {
   };
 
   const handleGoToHomePage = (): void => {
-    window.location.href = "http://https://dev-jobs-web-app-w6rq.vercel.app/";
+    window.location.href = "https://dev-jobs-web-app-w6rq.vercel.app/";
   };
 
   return (
